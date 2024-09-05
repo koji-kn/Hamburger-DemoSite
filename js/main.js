4025c533c471b3784a65e9d41714abb2d368bf36
kojikn <successruler@gmail.com>
2024-09-06 02:46:08 +0900

    index fin

--- js/main.js
+++ js/main.js
@@ -25,22 +25,22 @@
 
 
     //メインヴィジュアルカルーセル
-    const swiper = new Swiper('.swiper-container', {
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true,
-        },
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: true
-        },
-    });
+    // const swiper = new Swiper('.swiper-container', {
+    //     navigation: {
+    //         nextEl: '.swiper-button-next',
+    //         prevEl: '.swiper-button-prev',
+    //     },
+    //     loop: true,
+    //     pagination: {
+    //         el: '.swiper-pagination',
+    //         type: 'bullets',
+    //         clickable: true,
+    //     },
+    //     autoplay: {
+    //         delay: 3000,
+    //         disableOnInteraction: true
+    //     },
+    // });
 
     //画像インビュー
     $(window).on('scroll',function(){
@@ -55,27 +55,6 @@
     });
 
     $(window).on('load',function(){
-        $('.inview-back').each(function(){
-            let targetPosition = $(this).offset().top;
-            let scroll = $(window).scrollTop();
-            let windowHeight = $(window).height();
-            if(scroll > targetPosition - windowHeight){
-                $(this).addClass('show');
-            }
-        });
-    });
-    $(window).on('scroll',function(){
-        $('.inview-back').each(function(){
-            let targetPosition = $(this).offset().top;
-            let scroll = $(window).scrollTop();
-            let windowHeight = $(window).height();
-            if(scroll > targetPosition - windowHeight){
-                $(this).addClass('show');
-            }
-        });
-    });
-
-    $(window).on('load',function(){
         $('.inview').each(function(){
             let targetPosition = $(this).offset().top;
             let scroll = $(window).scrollTop();
@@ -85,5 +64,19 @@
             }
         });
     });
+
+    //ページ推移アニメーション
+
+    $(function(){
+        $('body').fadeMover({
+            'effectType': 1,     //1:フェードイン&&フェードアウト　2:フェードインのみ　3:フェードアウトのみ
+            'inSpeed': 300,      //フェードインのスピード
+            'outSpeed': 300,     //フェードアウトのスピード
+            'inDelay' : '0',     //フェードイン遅延スピード。複数要素がある場合、順番にフェードイン。0で遅延なし。
+            'outDelay' : '0',    //フェードアウト遅延スピード。複数要素がある場合、順番にフェードイン。0で遅延なし。
+            'nofadeOut' : 'nonmover'  //ページ内リンクの指定をしている<a>タグはフェードアウト動作禁止。それ以外の<a>タグでフェードアウト動作させたくない場合のclass名を指定。
+        });
+    });
+
 
 }
